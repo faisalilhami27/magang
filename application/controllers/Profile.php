3fde6ba8530698a5680c886b7449496d27cfd00c
Muhamad Faisal Ilhami Akbar <mfaisal2797@gmail.com>
2021-09-15 11:54:35 +0700

    update terbaru

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -22,7 +22,9 @@ class Profile extends CI_Controller
 	function profile($id)
 	{
 		$row = $this->User_model->get_by_id($id);
-		if ($row) :
+		$data = null;
+
+		if ($row) {
 			$data = array(
 				'action' => site_url('profile/update_action1'),
 				'id_users' => set_value('id_users', $row->id_users),
@@ -33,7 +35,7 @@ class Profile extends CI_Controller
 				'level' => set_value('id_user_level', $row->nama_level),
 				'is_aktif' => set_value('is_aktif', $row->is_aktif),
 			);
-		endif;
+		}
 		$this->template->load("template", "profile", $data);
 	}
 
@@ -53,13 +55,12 @@ class Profile extends CI_Controller
 		$this->_rules();
 		$foto = $this->upload_foto();
 		if ($this->form_validation->run() == FALSE) {
-			$this->update($id);
+			$this->profile($id);
 		} else {
 			if ($foto['file_name'] == '') {
 				$data = array(
 					'full_name' => $this->input->post('full_name', TRUE),
 					'email' => $this->input->post('email', TRUE),
-					'id_user_level' => $this->input->post('id_user_level', TRUE),
 					'is_aktif' => $this->input->post('is_aktif', TRUE));
 			} else {
 				$path = "assets/foto_profil/" . $row->images;
@@ -68,7 +69,6 @@ class Profile extends CI_Controller
 					'full_name' => $this->input->post('full_name', TRUE),
 					'email' => $this->input->post('email', TRUE),
 					'images' => $foto['file_name'],
-					'id_user_level' => $this->input->post('id_user_level', TRUE),
 					'is_aktif' => $this->input->post('is_aktif', TRUE));
 
 				// ubah foto profil yang aktif
@@ -84,12 +84,6 @@ class Profile extends CI_Controller
 	{
 		$this->form_validation->set_rules('full_name', 'full name', 'trim|required');
 		$this->form_validation->set_rules('email', 'email', 'trim|required');
-		//$this->form_validation->set_rules('password', 'password', 'trim|required');
-		//$this->form_validation->set_rules('images', 'images', 'trim|required');
-		$this->form_validation->set_rules('id_user_level', 'id user level', 'trim|required');
-		$this->form_validation->set_rules('is_aktif', 'is aktif', 'trim|required');
-
-		$this->form_validation->set_rules('id_users', 'id_users', 'trim');
 		$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
 	}
 }
